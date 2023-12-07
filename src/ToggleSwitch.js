5b45c493f05642b3de6d3136e48d4e7230851e5d
GyancarlosPinto <gyancarlospinto@pursuit.org>
2023-12-07 00:06:58 -0500

    Updated Button

--- src/ToggleSwitch.js
+++ src/ToggleSwitch.js
@@ -1,11 +1,14 @@
 import React,  {useState} from "react";
+import "./ToggleSwitch.css"
 
 function ToggleSwitch() {
     const [toggle, setSwitch] = useState(true)
     return (
         <div>
         <h2>Switch</h2>
-        <button onClick={() => setSwitch(!toggle)}>{toggle ? `ON` : `OFF`}</button>
+        <div onClick={() => setSwitch (!toggle)} className={`slider-container ${toggle ? "on" : "off"}`}>
+            <div className="slider">{toggle ? `ON` : `OFF`}</div>
+        </div>
         </div>
     );
 }
