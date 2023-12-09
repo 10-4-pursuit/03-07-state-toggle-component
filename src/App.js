7b5112a79c3c7d45f1eed23db72af61bd8fee0c7
EdgarCollado <edgarcollado@pursuit.org>
2023-12-09 18:03:11 -0500

    added-toggle-switch-component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ToggleOnToggleOff from './ToggleSwitch';
+
+function App() {
+  return (
+    <div className="App">
+      <ToggleOnToggleOff/>
+    </div>
+  );
+}
+
+export default App;
