b514f2446669d0760307379de467e6b703e91d8c
USERNAME <EMAIL>
2023-12-15 21:44:18 -0500

    all task completed, add CSS pages to design page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import ToggleSwitch from './ToggleSwitch';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className='App'>
+      
+      <ToggleSwitch />
+      
+    </div>
+  );
+}
+
+export default App;
