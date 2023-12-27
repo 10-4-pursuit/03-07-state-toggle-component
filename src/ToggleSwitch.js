e475166659c7a869e891b9e7509880d114acf929
godcindy <cindygodoy@pursuit.org>
2023-12-26 21:56:30 -0500

    add toggleswitch update

--- src/ToggleSwitch.js
+++ src/ToggleSwitch.js
@@ -1,17 +1,15 @@
 import React from "react";
 
 function ToggleSwitch() {
-  const[count, setCount] = React.useState('on');
+  const [count, setCount] = React.useState("on");
 
   return (
     <div>
-    <h2>{count}</h2>
-  <button onClick= {() => setCount ('off')}>OR</button>
+      <h2>{count}</h2>
+      <button onClick={() => setCount("off")}>Off</button>
+      <button onClick={() => setCount("on")}>On</button>{" "}
     </div>
   );
 }
 
-
 export default ToggleSwitch;
-
-
